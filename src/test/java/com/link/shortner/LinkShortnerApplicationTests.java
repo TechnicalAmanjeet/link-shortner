6248503041d52cf7e4de395574ea34486dfc5ef6
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2025-05-22 01:02:18 +0530

    first commit setup project

---
+++ src/test/java/com/link/shortner/LinkShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.shortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
